3933e310d26d61a4c34578c0723ee900b271d6eb
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2019-01-19 13:58:52 -0600

    Added inline comments

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -25,33 +25,34 @@ class App extends Component {
 
   constructor() {
     super();
+    // With immer, this.state and its nested structures can stay JS Objects
+    // and Arrays. No need for exotic new types or methods!
     this.state = this.initialState;
-    this.doubleThisRecipeForThursday = this.doubleThisRecipeForThursday.bind(
-      this
-    );
-    this.getShoppingList = this.getShoppingList.bind(this);
   }
 
   componentDidMount() {
     this.getShoppingList();
   }
 
-  getShoppingList() {
+  getShoppingList = () => {
     http.getShoppingList().then(result => {
       this.setState(result);
     });
-  }
+  };
 
   // Returns new ingredients array with doubled amounts
-  doubleTheIngredients(ingredients) {
+  doubleTheIngredients = ingredients => {
     return ingredients.map(ingredient => ({
       ...ingredient,
       number: ingredient.number * 2
     }));
-  }
+  };
 
-  doubleThisRecipeForThursday(day) {
+  doubleThisRecipeForThursday = day => {
     this.setState(
+      // We use immer's produce() function to allow us to make direct
+      // mutations on a draft state. Theese are then reconciled by immer
+      // to yield a new state object.
       produce(draft => {
         draft.recipes.Thursday = {
           name: 'Double ' + draft.recipes[day].name,
@@ -59,7 +60,7 @@ class App extends Component {
         };
       })
     );
-  }
+  };
 
   render() {
     const { destination, recipes } = this.state;
